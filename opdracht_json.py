913ffbc67956c1ea31f2ba1590557c158c192865
DaphneGroot <daphnegroot1996@hotmail.com>
2015-03-03 18:05:52 +0100

    Opdracht waarbij gekeken moet worden voor welke talen de woorden bloed en sterven gelijk zijn

---
+++ opdracht_json.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#Daphne Groot
+
+from collections import namedtuple
+import json
+
+def main():
+	""" Opend het bestand blood-die.json en zoekt de talen waarbij het woord voor 'bloed'
+	overeenkomt met het woord voor 'sterevn' """
+	
+	lijstOvereenkomsten = []
+	LIJST = namedtuple('LIJST','taalNaam , classificatie')
+	
+	
+	#bestand wordt geopend
+	with open('blood-die.json') as json_bestand:
+		bestand = json.load(json_bestand)
+		json_bestand.close()
+		
+		
+	#Voor iedere regel in het bestand wordt gekeken of de gegevens overeenkomen en dan worden ze 
+	#toegevoegd aan een nieuwe lijst
+	for regel in bestand:
+		lijstBloed = regel[2].split()
+		lijstSterven = regel[3].split()
+		
+		
+		gegevens = LIJST(regel[0], regel[1])
+		[lijstOvereenkomsten.append(gegevens) for item in lijstBloed if item in lijstSterven]
+		
+		
+		#for item in lijstBloed:
+		#	if item in lijstSterven:
+		#		gegevens = LIJST(regel[0])
+		#		lijstOvereenkomsten.append(gegevens)
+
+
+	[print(item) for item in lijstOvereenkomsten]
+
+
+if __name__ == "__main__":
+	main()
